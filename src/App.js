dcc542ca5041e5ff75c9edab736d4cbe94d5e560
wilmerAndresj <113381118+wilmerAndresj@users.noreply.github.com>
2023-05-15 16:54:07 -0500

    tareas

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import FreeCodeCampLogo from './imagenes/FreeCodeCamp_logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas'
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      
+      
+      <div className='tareas-lista-principal'>
+        <h1> Mis Tareas </h1>
+        <ListaDeTareas/>
+      </div>               
+
+    </div>
+  );
+}
+
+export default App;
+    
+
+
+
+       
+
+      
---
+++ src/componentes/TareaFormulario.js
@@ -0,0 +1,50 @@
+import React,{ useState } from "react";
+import '../hojas-de-estilo/TareaFormulario.css';
+import { v4 as uuidv4 } from 'uuid'
+
+function TareaFormulario(props) {
+
+    const [input, setInput] = useState();
+
+
+    const manejarCambio  = e =>{
+      setInput(e.target.value);
+      console.log(e.target.value);
+    }
+
+    
+    const manejarEnvio = e => {
+        e.preventDefault();
+        console.log("Enviando formulario");
+        
+        const tareaNueva = {
+            id: uuidv4() ,
+            texto: input,
+            completada: false
+        }
+
+        props.onSubmit(tareaNueva)
+    }
+
+
+
+    return (
+        <form 
+            className="tarea-formulario"
+            onSubmit={manejarEnvio}>
+            <input
+            className="tarea-input"
+            type='text'
+            placeholder="Escribe una Tarea"
+            name='texto'
+            onChange={manejarCambio}
+            />
+            <button className="tarea-boton">
+                Agregar Tarea
+            </button>
+
+        </form>
+    )
+}
+
+export default TareaFormulario;
